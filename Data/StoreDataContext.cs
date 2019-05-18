95cad3ca28ba74ba0d611ba58eee970f360801f5
pedronet5 <pedronet5@gmail.com>
2019-05-18 17:03:12 -0300

    Postar Projeto GitHub

---
+++ Data/StoreDataContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductCatalog.Data.Maps;
+using ProductCatalog.Models;
+
+namespace ProductCatalog.Data
+{
+    public class StoreDataContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuider)
+        {
+            optionsBuider.UseSqlServer(@"Server=localhost;Database=prodcat;Integrated Security=true;");
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfiguration(new ProductMap());
+            builder.ApplyConfiguration(new CategoryMap());
+        }
+
+    }
+}
